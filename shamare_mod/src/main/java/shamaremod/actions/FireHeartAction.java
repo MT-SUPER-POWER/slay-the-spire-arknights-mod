2045622ed482e6037154f110e60a951671fe3b1c
MT-SUPER-POWER <2065838115@qq.com>
2026-02-08 19:27:23 +0800

    清理多余的代码和编译文件

--- shamare_mod/src/main/java/shamaremod/actions/FireHeartAction.java
+++ shamare_mod/src/main/java/shamaremod/actions/FireHeartAction.java
@@ -13,15 +13,15 @@ public class FireHeartAction extends AbstractGameAction {
     private AbstractPlayer p;
     private boolean freeToPlayOnce;
     private int energyOnUse;
-    private boolean if_upgraded=false;
+    private boolean if_upgraded = false;
 
-    public FireHeartAction(AbstractPlayer p,  boolean freeToPlayOnce, int energyOnUse,boolean if_upgraded) {
+    public FireHeartAction(AbstractPlayer p, boolean freeToPlayOnce, int energyOnUse, boolean if_upgraded) {
         this.p = p;
         this.freeToPlayOnce = freeToPlayOnce;
         this.duration = Settings.ACTION_DUR_XFAST;
         this.actionType = AbstractGameAction.ActionType.SPECIAL;
         this.energyOnUse = energyOnUse;
-        this.if_upgraded=if_upgraded;
+        this.if_upgraded = if_upgraded;
     }
 
     @Override
@@ -34,21 +34,20 @@ public class FireHeartAction extends AbstractGameAction {
             effect += 2;
             this.p.getRelic("Chemical X").flash();
         }
-        if(this.if_upgraded){
-            effect+=1;
+        if (this.if_upgraded) {
+            effect += 1;
         }
 
         if (effect > 0) {
             int handSize = p.hand.size();
-            int exhaustCount = Math.min(handSize, effect); 
-           
-            //exhaust
+            int exhaustCount = Math.min(handSize, effect);
+
+            // exhaust
             addToBot(new ExhaustAction(exhaustCount, false, true, true));
-            //take effect  
-            addToBot(new MakeTempCardInDrawPileAction(new FireDisaster(), exhaustCount ,true, true));
-             // 回复生命值
+            // take effect
+            addToBot(new MakeTempCardInDrawPileAction(new FireDisaster(), exhaustCount, true, true));
+            // 回复生命值
             addToBot(new com.megacrit.cardcrawl.actions.common.HealAction(p, p, exhaustCount));
-            
 
             if (!this.freeToPlayOnce) {
                 this.p.energy.use(EnergyPanel.totalCount);
